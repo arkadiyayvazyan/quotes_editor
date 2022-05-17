e246c93bec7271e9a82a5238e67264640be83e59
Arkadiy Ayvazyan <arkadiy.ayvazyan@gmail.com>
2022-05-17 16:33:39 -0400

    turbo frames for date time items (started)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
     def form_error_notification(object)
         if object.errors.any?
             tag.div class: "error-message" do
-                object.errors.full_messages.to_sentence.capitilize
+                object.errors.full_messages.to_sentence.capitalize
             end
         end
     end
