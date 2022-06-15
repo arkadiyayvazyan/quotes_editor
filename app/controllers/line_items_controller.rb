fed9464a909a61c579b41f2b039d8f076195b154
Arkadiy Ayvazyan <arkadiy.ayvazyan@gmail.com>
2022-06-15 16:49:25 -0400

    line_items CRUD

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,8 +1,9 @@
 # app/contrrollers/line_item_controller.rb
 
-class LineItemController < ApplicationController
+class LineItemsController < ApplicationController
     before_action :set_quote
     before_action :set_line_item_date
+    before_action :set_line_item, only: [:edit, :update, :destroy]
 
     def new
         @line_item = @line_item_date.line_items.build
@@ -18,10 +19,21 @@ class LineItemController < ApplicationController
         end
     end
 
+    def edit
+    end
+
+    def update
+        if @line_item.update(line_item_params)
+            redirect_to quote_path(@quote), notice: "Item was sucessfully updated."
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
     private
 
     def line_item_params
-        params.require(:line_item).premit(:name, :description, :quantity, :unit_price)
+        params.require(:line_item).permit(:name, :description, :quantity, :unit_price)
     end
 
     def set_quote
